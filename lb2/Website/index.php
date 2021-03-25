6a687269b373ab49b7f8a41a792814d1262bb5ff
Levo092208 <78599375+Levo092208@users.noreply.github.com>
2021-03-25 15:39:49 +0100

    Website

---
+++ lb2/Website/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+	<meta charset="UTF-8">
+	<title>PHP Website M300</title>
+	<link href="style.css" rel="stylesheet">
+</head>
+
+<body>
+
+</body>
+</html>
