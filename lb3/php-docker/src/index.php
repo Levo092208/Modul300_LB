d73df9fde892cc509e8d7a2a4875089d79454885
Levo092208 <78599375+Levo092208@users.noreply.github.com>
2021-04-19 10:51:58 +0200

    Dcoker-compose up -> own db with not really usable tabels

--- lb3/php-docker/src/index.php
+++ lb3/php-docker/src/index.php
@@ -1,3 +1,29 @@
 <?php
 
-echo "Hello from the docker container. It is not finishes by now, more stuff will come soon. Dockercompose working?";
+echo "Hello from the docker m300 container";
+
+$mysqli = new mysqli("db", "root", "example", "M300");
+
+$sql = "INSERT INTO users (name, fav_color) VALUES('Levo', 'blau')";
+$result = $mysqli->query($sql);
+$sql = "INSERT INTO users (name, fav_color) VALUES('Albin', 'grün')";
+$result = $mysqli->query($sql);
+$sql = "INSERT INTO users (name, fav_color) VALUES('Das ist ein Test funktionierts?', 'mais oui')";
+$result = $mysqli->query($sql);
+$sql = "INSERT INTO users (name, fav_color) VALUES('burim', 'weiss')";
+$result = $mysqli->query($sql);
+
+
+$sql = 'SELECT * FROM users';
+
+if ($result = $mysqli->query($sql)) {
+    while ($data = $result->fetch_object()) {
+        $users[] = $data;
+    }
+}
+
+foreach ($users as $user) {
+    echo "<br>";
+    echo $user->name . " " . $user->fav_color;
+    echo "<br>";
+}
